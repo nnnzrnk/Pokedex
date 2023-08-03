483a0e6c6db087fdc7b77a751dbb02f78b00e58f
nnnzrnk <127133520+nnnzrnk@users.noreply.github.com>
2023-08-03 23:27:50 +0200

    completed task 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-var favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Mantine', height: 2.1, types:['water','flying']},
+    {name: 'Nidoking', height: 1.4, types:['ground','poison']},
+    {name: 'Zekrom', height: 2.9, types: ['dragon', 'electric']}
+ ]
+ 
