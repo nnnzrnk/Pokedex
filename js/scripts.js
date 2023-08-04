98c30c7a982ac14c64a5349b8392cd51861d6eba
nnnzrnk <127133520+nnnzrnk@users.noreply.github.com>
2023-08-04 18:06:54 +0200

    completed task 1.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 let pokemonList = [
-    {name: 'Mantine', height: 2.1, types:['water','flying']},
-    {name: 'Nidoking', height: 1.4, types:['ground','poison']},
-    {name: 'Zekrom', height: 2.9, types: ['dragon', 'electric']}
- ]
- 
+  { name: "Mantine", height: 2.1, types: ["water", "flying"] },
+  { name: "Nidoking", height: 1.4, types: ["ground", "poison"] },
+  { name: "Zekrom", height: 2.9, types: ["dragon", "electric"] },
+]
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) `)
+    
+    if (pokemonList[i].height > 2.5) {
+        document.write(` ${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that’s big!`)
+    } else {
+        document.write(` ${pokemonList[i].name} (height: ${pokemonList[i].height}) `)
+    }
+}
+
+
