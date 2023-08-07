f91755fa11b4c6129ad15c6efebe953375c36a9c
nnnzrnk <127133520+nnnzrnk@users.noreply.github.com>
2023-08-07 18:11:33 +0200

    completed Task 1.5 Part 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,35 @@
+
 let pokemonList = [
   { name: "Mantine", height: 2.1, types: ["water", "flying"] },
   { name: "Nidoking", height: 1.4, types: ["ground", "poison"] },
   { name: "Zekrom", height: 2.9, types: ["dragon", "electric"] },
 ]
 
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) `)
-    
-    if (pokemonList[i].height > 2.5) {
-        document.write(` ${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that’s big!`)
-    } else {
-        document.write(` ${pokemonList[i].name} (height: ${pokemonList[i].height}) `)
-    }
-}
+//Task 1.3
 
+// for (let i = 0; i < pokemonList.length; i++) {
+//     document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) `)
+    
+//     if (pokemonList[i].height > 2.5) {
+//         document.write(` ${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that’s big!`)
+//     } else {
+//         document.write(` ${pokemonList[i].name} (height: ${pokemonList[i].height}) `)
+//     }
+// }
+
+
+
+//completed Task 1.5 
+//First way:
+
+pokemonList.forEach(function(pokemons){
+    console.log(`${pokemons.name} has height ${pokemons.height} and its types are ${pokemons.types} `)
+});
+
+//Second way:
+
+function getPokemonsInfo(pokemons){
+    console.log(`${pokemons.name} has height ${pokemons.height} and its types are ${pokemons.types} `)
+}
+pokemonList.forEach(getPokemonsInfo)
 
